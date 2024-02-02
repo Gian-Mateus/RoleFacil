0169a029a1a7b803517d458608f835a348d82a2b
Gian Mateus <gian.m.silver@gmail.com>
2024-02-02 10:59:38 -0300

    att: bootstrap instalado e funcionando

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <title>Role Fácil</title>
+</head>
+<body>
+    <h1>Role Fácil</h1>
+
+    <button class="btn btn-success">Button</button>
+</body>
+</html>
