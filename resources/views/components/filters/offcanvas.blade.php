fe4318bfb8df4447d4185f5390064e42df4e404b
Gian Mateus <gian.m.silver@gmail.com>
2024-02-20 08:26:00 -0300

    ajustando background e personalizando cores do accordion e offcanvas

--- resources/views/components/filters/offcanvas.blade.php
+++ resources/views/components/filters/offcanvas.blade.php
@@ -1,7 +1,7 @@
-<div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasFilters" aria-labelledby="offcanvasFiltersLabel">
+<div class="offcanvas offcanvas-start data-bs-scroll="true"  tabindex="-1" id="offcanvasFilters" aria-labelledby="offcanvasFiltersLabel">
         <div class="offcanvas-header">
             <h5 class="offcanvas-title" id="offcanvasFiltersLabel">Filtros</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            <button type="button" class="btn-close " data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
         <div class="offcanvas-body">
             <x-filters.accordion/>
