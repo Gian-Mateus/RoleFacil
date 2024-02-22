945eb54d0058031958d6862f67da0340992f021a
Bruno Chiavenato <lopes928@gmail.com>
2024-02-22 17:07:37 -0300

    listas e cards adicionados a pagina freelance

--- resources/views/freelancer.blade.php
+++ resources/views/freelancer.blade.php
@@ -1,9 +1,21 @@
 @extends('layout')
 @section('content')    
-<x-filters.offcanvas/>
+{{-- <x-filters.offcanvas/>
 <button class="btn btn-primary ms-2 mt-2 border" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasFilters"
     aria-controls="offcanvasFilters" id="collapse-settings">
     <x-svg.settings/>
-</button>
-  <h1>Lista de Freelancer</h1>
+</button> --}}
+        <div class="text-center">
+          <h2 class="fw-medium fs-1">Lista de Freelances</h2>
+          <h3>Encontre aqui uma oportunidade de Freelance que se encaixe com seu perfil!</h3>
+        </div>
+
+        {{-- lista de cards --}}
+      <div class="event-list-cards row">
+
+        <x-freelance-list aboutWhat="Zona"/>
+        <x-freelance-list aboutWhat="Pirokada no Daniel"/>
+        <x-freelance-list aboutWhat="Pub"/>
+        
+    </div>
 @endsection
