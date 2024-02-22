3f9d2986a06459347ffdc0c4a20e8b3e41198ff8
Gian Mateus <gian.m.silver@gmail.com>
2024-02-21 21:21:01 -0300

    ajust navbar-collapse fix flex grow 0

--- app/View/Components/carousel.php
+++ app/View/Components/carousel.php
@@ -12,7 +12,6 @@ class carousel extends Component
      * Create a new component instance.
      */
     public function __construct(
-        public string $classCarousel,
         public string $idCarousel
     )
     {
