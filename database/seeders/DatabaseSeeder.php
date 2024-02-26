2101c3c13e292e579625b056f1355966fe218365
Merlin <gor461@gmail.com>
2024-02-26 20:41:50 -0300

    testando

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,10 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         $this->call([
+            FreelanceSeeder::class,
             CliEstablishmentSeedSeeder::class,
+            EventSeeder::class,
+            /* UserSeeder::class, */
         ]);
     }
 }
