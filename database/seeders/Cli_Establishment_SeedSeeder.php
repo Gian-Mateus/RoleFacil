f5bb42ff1f15907fb8d865b4dcd42d6e5ece8ed6
Bernardo <bernardoroepke2704@gmail.com>
2024-02-26 14:05:22 -0300

    alterações banco de dados

---
+++ database/seeders/Cli_Establishment_SeedSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class Cli_Establishment_SeedSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
