53bcd0746d47c8d4749936eb0b93706ea5bcb889
Merlin <gor461@gmail.com>
2024-02-26 17:43:21 -0300

    Documentando o footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,15 +2,19 @@
     <div class="row">
         <div class="col">
             <ul>
+                {{-- FOOTER - LINK DO FACEBOOK --}}
                 <li>
                     <a href="https://www.facebook.com/profile.php?id=61557044931069"><x-svg.social.facebook/></a> 
                 </li>
+                {{-- FOOTER - LINK DO WHATSAPP --}}
                 <li>
                     <a href="https://wa.me/5548999576037"><x-svg.social.whatsapp/></a> 
                 </li>
+                {{-- FOOTER - INSTAGRAM --}}
                 <li>
                     <a href="https://www.instagram.com/rolefacil/"><x-svg.social.instagram/></a>
                 </li>
+                {{-- FOOTER - TWITTER --}}
                 <li>
                     <a href="https://twitter.com/rolefacil"><x-svg.social.twiter/></a>
                 </li>
