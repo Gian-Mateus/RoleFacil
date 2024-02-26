d30a9b27b545d398c2047fb6b81427c8a44c7775
Bernardo <bernardoroepke2704@gmail.com>
2024-02-25 21:05:22 -0300

    testando freelancers

--- database/migrations/2024_02_22_173738_create_freelancers_table.php
+++ database/migrations/2024_02_22_173738_create_freelancers_table.php
@@ -19,10 +19,10 @@ return new class extends Migration
             $table->text('freelancer_description');
             $table->string('freelancer_start', 20);
             $table->string('freelancer_end', 20);
-            $table->string('freelancer_money', 20);
+            $table->string('freelancer_money', 20)->nullable();
             $table->string('freelancer_email', 255);
-            $table->string('freelancer_phone', 11);
-            $table->string('freelancer_telefone_fixo', 10);
+            $table->string('freelancer_phone', 11)->nullable();
+            $table->string('freelancer_telefone_fixo', 10)->nullable();
             $table->boolean('freelancer_active');
             $table->timestamps();
         });
