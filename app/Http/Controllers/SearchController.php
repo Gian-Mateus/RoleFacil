ec325967fbc1bc187b48764db9cd0023ff0debbc
Bernardo <bernardoroepke2704@gmail.com>
2024-02-27 18:06:24 -0300

    teste de pesquisa

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\CliEstablishmentSeed;
+use DB;
 
 class SearchController extends Controller
 {
@@ -15,60 +16,83 @@ class SearchController extends Controller
         return view('search');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        $establishments = CliEstablishmentSeed::where('cli_establishment_seed_city', 'like',  '%'.$request->search.'%')
-        ->orWhere('cli_establishment_seed_categories', 'like', '%'.$request->search.'%')
-            ->orWhere('cli_establishment_seed_name', 'like', '%'.$request->search.'%')
-            ->orWhere('cli_establishment_seed_address_formatted', 'like', '%'.$request->search.'%')
-            ->orWhere('cli_establishment_seed_neighborhood', 'like', '%'.$request->search.'%')
+        function buscarTudoRelacionado($palavra) {
+
+            $resultados = CliEstablishmentSeed::where('cli_establishment_seed_categories', 'like', "%$palavra%")
+            ->orwhere('cli_establishment_seed_city', 'like', "%$palavra%")
+            ->orwhere('cli_establishment_seed_name', 'like', "%$palavra%")
+            ->orwhere('cli_establishment_seed_address_formatted', 'like', "%$palavra%")
             ->get();
+
+            return $resultados;
             
-            return view('search', ['establishments' => $establishments]);
-    }
+        }
 
-        
+        function buscarComRelacao($palavras) {
+            $palavra1 = $palavras[0];
+            $palavra2 = $palavras[1];
+            $resultados = DB::select("SELECT * FROM cli_establishment_seeds WHERE cli_establishment_seed_categories LIKE '%$palavra1%' AND cli_establishment_seed_city LIKE '%$palavra2%'");
+            //var_dump($resultados);
+            return $resultados;
+        }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
+        function alterarNomenclatura($palavra) {
+            switch($palavra) {
+                case 'padaria':
+                    $palavra = 'bakery';
+                    break;
+                case 'padarias':
+                    $palavra = 'bakery';
+                    break;
+                case 'restaurante':
+                    $palavra = 'restaurant';
+                    break;
+                case 'restaurantes':
+                    $palavra = 'restaurant';
+                    break;
+                case 'comida':
+                    $palavra = 'food';
+                    break;
+                case 'café':
+                    $palavra = 'cafe';
+                    break;
+                case 'cafés':
+                    $palavra = 'cafe';
+                    break;
+                case 'mercado':
+                    $palavra = 'store';
+                    break;
+                case 'mercados':
+                    $palavra = 'store';
+                    break;
+                case 'bares':
+                    $palavra = 'bar';
+                    break;
+            }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
+            return $palavra;
+        }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
+        $palavras_irrelevantes = ['em', 'na', 'no', 'dos', 'das', 'do', 'a', 'o'];
+        $palavras = explode(' ', $request->search);
+        $palavras_filtradas = array_diff($palavras, $palavras_irrelevantes);
+        $palavras_filtradas = array_values($palavras_filtradas);
+        $palavras_alteradas = [];
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        if (count($palavras_filtradas) === 1) {
+            $palavra_alterada = alterarNomenclatura($palavras_filtradas[0]);
+            $resultados = buscarTudoRelacionado($palavra_alterada);
+        } else {
+            foreach($palavras_filtradas as $palavra) {
+                $palavra_alterada = alterarNomenclatura($palavra);
+                array_push($palavras_alteradas, $palavra_alterada);
+            }
+
+            $resultados = buscarComRelacao($palavras_alteradas);
+        }
+
+        return view('search', ['resultados' => $resultados]);
     }
 }
