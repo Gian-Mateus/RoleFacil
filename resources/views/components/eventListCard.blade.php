e9f8d34e53c2c8544e2f4aef2bbd948647421cd6
Merlin <gor461@gmail.com>
2024-02-27 11:51:06 -0300

    ajustando2

--- resources/views/components/eventListCard.blade.php
+++ resources/views/components/eventListCard.blade.php
@@ -2,7 +2,6 @@
     <div class="head-row d-flex justify-content-between mt-3">
         <h2 class="text-white fw-semibold">{{ $aboutWhat }}</h2>
         <span class="align-self-end"><a href="#">Ver mais...</a></span>
-        <span class="align-self-end"><a href="# ">Ver mais...</a></span>
     </div>
     <div class="row-col cards-events">
 
