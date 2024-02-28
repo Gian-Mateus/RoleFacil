915441347a83254b2b7a143ca4d2966ea5cfd47b
Bernardo <bernardoroepke2704@gmail.com>
2024-02-27 23:28:35 -0300

    registercontroller

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -20,19 +20,19 @@ class RegisterController extends Controller
      */
     public function store(Request $request)
     {
-        $api_key = "AIzaSyCFOe8jC0NWPr-qG-55j7II02ZhoBwfKLo";
-        $address_alterado = str_replace(" ", "+", $request->address);
-        $city_alterado = str_replace(" ", "+", $request->city);
-        $neighborhood_alterado = str_replace(" ", "+", $request->neighborhood);
-        $localidade = $address_alterado .'+'. $request->number_address .'+'. $city_alterado .'+'. $neighborhood_alterado;
-        $base_url = "https://maps.googleapis.com/maps/api/geocode/json?key={$api_key}&address={$localidade}&components=coutry%3ABR";
-        $response = json_decode(file_get_contents($base_url));
-        $caminho1 = $response->results;
-        $caminho2 = $caminho1[0];
-        $caminho3 = $caminho2->geometry;
-        $caminho4 = $caminho3->location;
-        $lat = $caminho4->lat;
-        $lng = $caminho4->lng;
+        // $api_key = "AIzaSyCFOe8jC0NWPr-qG-55j7II02ZhoBwfKLo";
+        // $address_alterado = str_replace(" ", "+", $request->address);
+        // $city_alterado = str_replace(" ", "+", $request->city);
+        // $neighborhood_alterado = str_replace(" ", "+", $request->neighborhood);
+        // $localidade = $address_alterado .'+'. $request->number_address .'+'. $city_alterado .'+'. $neighborhood_alterado;
+        // $base_url = "https://maps.googleapis.com/maps/api/geocode/json?key={$api_key}&address={$localidade}&components=coutry%3ABR";
+        // $response = json_decode(file_get_contents($base_url));
+        // $caminho1 = $response->results;
+        // $caminho2 = $caminho1[0];
+        // $caminho3 = $caminho2->geometry;
+        // $caminho4 = $caminho3->location;
+        // $lat = $caminho4->lat;
+        // $lng = $caminho4->lng;
 
         $user = new User;
 
@@ -40,20 +40,6 @@ class RegisterController extends Controller
         $user->password = bcrypt($request->password);
         $user->user_name = $request->name;
         $user->user_cnpj = $request->cnpj;
-        $user->user_description = $request->description;
-        $user->user_zipcode = $request->zipcode;
-        $user->user_complement = $request->complement;
-        $user->user_address = $request->address;
-        $user->user_number_address = $request->number_address;
-        $user->user_city = $request->city;
-        $user->user_neighborhood = $request->neighborhood;
-        $user->user_uf = $request->uf;
-        $user->user_latitude = $lat;
-        $user->user_longitude = $lng;
-        $user->user_facebook_link = $request->facebook_link;
-        $user->user_instagram_link = $request->instagram_link;
-        $user->user_celular = $request->celular;
-        $user->user_telefone_fixo = $request->telefone_fixo;
 
         $user->save();
         return redirect('login')->with('msg', 'Usuário registrado com sucesso!');
