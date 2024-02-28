f389bdb2840e6236e8e27d61dda97656224de153
Claudio Elias <cnelias@gmailcom>
2024-02-28 08:27:25 -0300

    Quase no final

--- app/Http/Controllers/admin/LoginController.php
+++ app/Http/Controllers/admin/LoginController.php
@@ -17,7 +17,7 @@ class LoginController extends Controller
         //atenticando
         if(Auth::attempt($credentials)){
             $request->session()->regenerate();
-            return redirect()->intended("/");
+            return redirect()->intended("/estabelecimentos");
         }
 
         return back()->withErrors(["erro" => "E-mail ou senha inválidos."]);
