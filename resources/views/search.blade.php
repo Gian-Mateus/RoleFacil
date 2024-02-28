de25908fa6b6ece8d2405ad49739d387d635ad5b
Gian Mateus <gian.m.silver@gmail.com>
2024-02-28 12:00:55 -0300

    card list quando faz uma pesquisa

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,10 +1,19 @@
 @extends('layout')
 @section('content')   
-<x-breadcrumb title="Freelancer" /> 
+<x-breadcrumb routeLinks="Pesquisa" /> 
         <div class="text-center">
           <h2 class="fw-medium fs-1">Pesquisa</h2>
         </div>
         @foreach($resultados as $resultado)
-          <p>{{ $resultado->cli_establishment_seed_name }}</p>
+        @php
+          $img = explode(";", $resultado["cli_establishment_seed_imgs"]);
+        @endphp
+        @component('components.card-list', [
+          "title" =>  $resultado["cli_establishment_seed_name"], 
+          "text" => $resultado["cli_establishment_seed_address_formatted"],
+          "img" => $img[0],
+          "establishment" => $resultado["cli_establishment_seed_id"]
+          ])
+          @endcomponent
         @endforeach
 @endsection
